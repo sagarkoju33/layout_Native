20f700eea61293f4f7bcd486a8ba6078dc19d98f
Sagar.koju <sagar.koju@mypay.com.np>
2025-03-17 15:09:17 +0545

    practise of card design

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,12 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val goButton = findViewById<Button>(R.id.send1)
         val goButton1 = findViewById<Button>(R.id.send2)
+        val goButton2 = findViewById<Button>(R.id.send3)
+        val goButton3 = findViewById<Button>(R.id.send4)
         goButton.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
         goButton1.setOnClickListener {
             val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+        goButton2.setOnClickListener {
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
+        goButton3.setOnClickListener {
+            val intent = Intent(this, MainActivity5::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/myapplication/MainActivity4.kt
+++ app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -1,16 +1,18 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivity4 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main4)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.name1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
